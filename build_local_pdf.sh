5269327d6c247a1eab5a02c311dacfec0906663d
JackH <jack@realtimeradio>
2023-10-17 17:01:46 +0100

    Make file with correct doc number

--- build_local_pdf.sh
+++ build_local_pdf.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-pdfname=D2k-00017-RCF-ICD-RCF_RCP_interface.pdf
+pdfname=D2k-00019-RCF-ICD-RCF_RCP_interface.pdf
 
 pandoc docs/*.md -o ${pdfname} -s -V colorlinks -V links-as-notes --number-sections --template template-pd2.9.latex 
